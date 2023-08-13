793b173185a1ab5a98ddc2c8779b102e96b4ef65
Yash Goyal <yashgo0018@gmail.com>
2023-08-13 18:58:38 +0530

    added @chainlink/contracts dependency

--- contracts/TokenStopLoss.sol
+++ contracts/TokenStopLoss.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
-import "./interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./interfaces/UniswapV2Interfaces.sol";
 import "./interfaces/TokenStopLossInterface.sol";
 
